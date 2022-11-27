026afb2354077163ba6072ae9364a6abdad65a61
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-11-27 12:18:39 -0500

    fix cors error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/app/:path*',
+        destination: 'https://app.pageblox.io/:path*',
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: false,
   images: {
