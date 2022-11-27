ba774f5ce026f43466faa44ac4d533e41e5359a5
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-11-27 12:29:13 -0500

    switch back to localhost

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/app/:path*',
-        destination: 'https://app.pageblox.io/:path*',
-      },
-    ]
-  },
   reactStrictMode: true,
   swcMinify: false,
   images: {
